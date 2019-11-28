ed0343ca792e00e3bb9b5af668d2a2c2e7d8b36c
Perturabo4 <perturabo40@gmail.com>
2019-11-28 14:44:27 +0200

    add Header and Movie components

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,10 @@
 import React from 'react';
+import Header from './Header';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-     
-    </div>
+    <Header />
   );
 }
 
