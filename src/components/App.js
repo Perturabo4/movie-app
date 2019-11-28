993628f1a54813a5dddcbae7aa0aac638eef3ab9
Perturabo4 <perturabo40@gmail.com>
2019-11-28 16:44:58 +0200

    add OMDb API to App

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,60 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Header from './Header';
+import Movie from './Movie';
+import Search from './Search';
 import './App.css';
 
+const MOVIE_API_URL = 'http://www.omdbapi.com/?s=man&apikey=44fdb66b';
+
 function App() {
+  const [loading, setLoading] = useState(true);
+  const [movies, setMovies] = useState([]);
+  const [errorMessage, setErrorMessage] = useState(null);
+
+  useEffect(() => {
+    fetch(MOVIE_API_URL)
+      .then(response => response.json())
+      .then(jsonResponse => {
+          setMovies(jsonResponse.Search);
+          setLoading(false);
+      });
+  }, []);
+
+  const search = searchValue => {
+    setLoading(true);
+    setErrorMessage(null);
+
+    fetch(`http://www.omdbapi.com/?s=${searchValue}&apikey=44fdb66b`)
+      .then(response => response.json())
+      .then(jsonResponse => {
+        if(jsonResponse.Response === 'True') {
+            setMovies(jsonResponse.Search);
+            setLoading(false);
+        } else {
+          setErrorMessage(jsonResponse.Error);
+          setLoading(false);
+        }
+      });
+  };
+
+
   return (
-    <Header />
+    <div className="App">
+      <Header text="HOOKED" />
+      <Search search={search} />
+      <p className="App-intro">
+        Sharing a few of our favorite movies
+      </p>
+      <div className="movies">
+        {loading && !errorMessage 
+          ? <span>Loading ...</span>
+          : errorMessage 
+              ? <div className="errorMessage">{errorMessage}</div>
+              : movies.map((movie, index) => (
+                <Movie key={`${index}-${movie.Title}`} movie={movie} />
+              ))}
+      </div>
+    </div>
   );
 }
 
