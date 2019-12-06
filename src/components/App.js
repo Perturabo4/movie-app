1da8f5650c72513d95668186fa8f3f47646664f5
Perturabo4 <perturabo40@gmail.com>
2019-12-06 12:55:31 +0200

    add loader folder

--- src/components/App.js
+++ src/components/App.js
@@ -2,7 +2,7 @@ import React, { useReducer, useEffect } from 'react';
 import Header from './Header';
 import Movie from './Movie';
 import Search from './Search';
-import Loader from './Loader';
+import Loader from './Loader/';
 import './App.css';
 
 const MOVIE_API_URL = 'http://www.omdbapi.com/?s=man&apikey=44fdb66b';
